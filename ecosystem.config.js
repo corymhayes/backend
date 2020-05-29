0a362acadfc8923bc4e2ac326fc46a8ce8e3044d
Cory Hayes <corymhayes@gmail.com>
2020-05-29 18:16:16 -0500

    added pm2 eco file

---
+++ ecosystem.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  app: [{
+    name: "api",
+    script: "./app.js",
+    env: {
+      "KNEX_HOST": "72.24.228.86",
+      "KNEX_PORT": 49170,
+      "KNEX_USER": "tadmin",
+      "KNEX_PASS": "test3698",
+      "KNEX_DB": "JCTDATA"
+    }
+  }]
+}
